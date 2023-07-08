bb650f1f7eef90caf48dfbae5dde169006567611
dduclayan <dduclayan@gmail.com>
2023-07-08 14:10:31 -0500

    Update main to accept YAML file as a flag

--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -2,12 +2,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 	"urlshort"
 )
 
+var (
+	file = flag.String("file", "", "path to yaml file")
+)
+
 func main() {
+	flag.Parse()
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -20,13 +27,12 @@ func main() {
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
 	// NOTE: no tabs allowed in yaml. spacing is very important.
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+
+	yaml, err := generateYAMLInput()
+	if err != nil {
+		panic(err)
+	}
+	yamlHandler, err := urlshort.YAMLHandler(yaml, mapHandler)
 	if err != nil {
 		panic(err)
 	}
@@ -38,6 +44,32 @@ func defaultMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", hello)
 	return mux
+}
+
+func generateYAMLInput() ([]byte, error) {
+	if *file != "" {
+		fmt.Printf("reading file %v\n", *file)
+		data, err := readFile(*file)
+		if err != nil {
+			panic(err)
+		}
+		return data, nil
+	}
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+	return []byte(yaml), nil
+}
+
+func readFile(filePath string) ([]byte, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
